65983b270a3ee74e6e334f25db442177f1f103e0
Duncan Kipkemoi <60089273+flawlesscode254@users.noreply.github.com>
2021-12-16 16:20:26 +0300

    removed password

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ os.environ["PATH"] += r"/usr/bin/geckodriver"
 driver.get("https://www.facebook.com")
 driver.implicitly_wait(30)
 one = driver.find_element_by_id("email").send_keys("0797373033")
-two = driver.find_element_by_id("pass").send_keys("hartabalin")
+two = driver.find_element_by_id("pass").send_keys("")
 # three = driver.find_element_by_id("u_0_h_O2").click()
 three = driver.find_element_by_xpath("// button[contains(text(),\'Log In')]").click()
 # one.click()
